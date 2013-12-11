d579c0a1b2b1fcf854ca11885f58ff55e77f2e1c
Tim Ruffles <timruffles@googlemail.com>
2013-12-11 15:39:59 +0200

    update

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = express()
 app.get("/add/:a/:b",function(req,res) {
   var a = parseInt(req.params.a)
   var b = parseInt(req.params.b)
-  if(isNaN(a) || isNaN(b)) return res.send(400,{error: "NaN"})
+  if(isNaN(a) || isNaN(b)) return res.send(400,{error: "NaN"});
   res.send({answer: a + b})
 })
 
